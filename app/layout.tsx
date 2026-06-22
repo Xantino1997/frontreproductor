ea1366b16dcc3619d0c0c778683bfa89236d2ce4
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2026-06-22 15:19:52 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import UpdateBaner from "./components/Player/UpdateBaner";
 import BotonDescarga from "./components/Player/BotonDescarga";
+import ConditionalShell from "./components/Player/ConditionalShell";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <UpdateBaner />
+        <ConditionalShell />
       </body>
     </html>
   );
